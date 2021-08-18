e6bdff168720157920db6f1f234f217338cdff1e
Lewis <hamiltonjlewis@gmail.com>
2021-08-18 15:43:08 -0700

    converted friday to hook

--- src/components/friday.js
+++ src/components/friday.js
@@ -1,47 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
-class Friday extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            startYear: '',
-            endYear: ''
-        }
-        this.startChange = this.startChange.bind(this);
-        this.endChange = this.endChange.bind(this);
-        this.fridayTheThirteenths = this.fridayTheThirteenths.bind(this);
-    }
+export default function Friday () {
+    const [startYear, setStartYear] = useState('');
+    const [endYear, setEndYear] = useState('');
 
-    startChange(event) {
-        this.setState({startYear: event.target.value});
-    }
-
-    endChange(event) {
-        this.setState({endYear: event.target.value});
-    }
-
-    fridayTheThirteenths() {
-        var startYear = new Date(this.state.startYear,0,1)
-        var endYear = new Date(this.state.endYear,11,31)
-        console.log(startYear + ' ' + endYear)
-    }
-    
-    render() {
     return(
         <div>
-        <Link to="/">Home</Link>
-        <a href="https://www.codewars.com/kata/540954232a3259755d000039/javascript">Challenge info</a>
-        <h1>Friday the 13th Calculator</h1>
-        <h2>Please enter your start and end years below:</h2>
-        <h3>Start Year</h3>
-        <input id="start" value={this.state.startYear} onChange={this.startChange}></input>
-        <h3>End Year</h3>
-        <input id="end" value={this.state.endYear} onChange={this.endChange}></input>
-        <button onClick={this.fridayTheThirteenths}>Calculate</button>
+            <Link to="/">Home</Link>
+            <a href="https://www.codewars.com/kata/540954232a3259755d000039/javascript">Challenge info</a>
+            <h1>Friday the 13th Calculator</h1>
+            <h2>Please enter your start and end years below:</h2>
+            <h3>Start Year</h3>
+            <input onChange={e => setStartYear(e.target.value)}></input>
+            <h3>End Year</h3>
+            <input onChange={e => setEndYear(e.target.value)}></input>
+            <button onClick={() => console.log(new Date(startYear) + ' ' + new Date(endYear))}>Calculate</button>
         </div>
     )
-    }
 }
-
-export default Friday;
