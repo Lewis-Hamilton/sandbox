6745358687489c20ecf5124dde27fb8bbefa6724
Lewis <hamiltonjlewis@gmail.com>
2021-08-18 15:48:31 -0700

    converted function to hook

--- src/components/function.js
+++ src/components/function.js
@@ -1,29 +1,15 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-function newFunction(name, age) {
-    return alert(name + ' is ' + age + ' years old.');
-}
-
-function myArray(number) {
-    var newArray = [];
-    var i = 0;
-
-    for(i = 1; i <= number; i++){
-    newArray.push(i);
+export default function Function() {
+    const newFunction=(name, age) => {
+        return alert(name + ' is ' + age + ' years old.');
     }
 
-    return newArray;
-}
-
-function Function() {
-    newFunction("Bill", 69);
-    myArray();
-    console.log(myArray(69));
     return (
-        <Link to="/">Home</Link>
+        <div>
+            <Link to="/">Home</Link>
+            <button onClick={() => newFunction('Bill', 69)}>Bill</button>
+        </div>
     )
-
 }
-
-export default Function;
