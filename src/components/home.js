8b0a4f9c03a7f8215bbccb5ac19a55e7a20995dc
Lewis <hamiltonjlewis@gmail.com>
2021-08-19 11:09:35 -0700

    updated homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
 
 const Home = () => {
-  return (
-    <ol>
-      <li>
-        <Link to="/function">Function</Link>
-      </li>
-      <li>
-        <Link to="/friday">Friday</Link>
-      </li>
-      <li>
-        <Link to="/input">Input</Link>
-      </li>
-      <li>
-        <Link to="/object">Object</Link>
-      </li>
-      <li>
-        <Link to="/score">Score</Link>
-      </li>
-      <li>
-        <Link to="/color">Color</Link>
-      </li>
-      <li>
-        <Link to="/array">Array</Link>
-      </li>
-    </ol>
-  );
+  return <Typography variant="h1">Welcome Home</Typography>;
 };
 
 export default Home;
